7c2023de0458a47722bb9f8e0fb682ac6ad8bd92
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2019-04-25 11:24:17 -0600

    add raw data

---
+++ src/raw_data.js
@@ -0,0 +1,73 @@
+export const miniMarketProducts = [
+  {
+    id: 1,
+    name: 'Mango',
+    description: 'Fresh mango',
+    price: 2
+  },
+  {
+    id: 2,
+    name: 'Aged Cheese',
+    description: 'Twenty year aged cheese',
+    price: 4
+  },
+  {
+    id: 3,
+    name: 'Watermelon',
+    description: 'Large Watermelon',
+    price: 6
+  },
+  {
+    id: 4,
+    name: 'Coca-Cola',
+    description: 'Refreshing Soda'
+  },
+  {
+    id: 5,
+    name: 'Twix Bar',
+    description: 'Tasty Twix',
+    price: 3
+  },
+  {
+    id: 6,
+    name: 'Sun Glasses',
+    description: 'Get out in the sun!',
+    price: 12
+  },
+  {
+    id: 7,
+    name: 'Sunscreen',
+    description: 'SPF 40',
+    price: 5
+  },
+  {
+    id: 8,
+    name: 'Towel',
+    description: 'Dry off fast',
+    price: 9
+  },
+  {
+    id: 9,
+    name: 'Beach Ball',
+    description: 'Play on the beach',
+    price: 13
+  },
+  {
+    id: 10,
+    name: 'Pro Cooler',
+    description: 'keep those snacks cool!',
+    price: 15
+  },
+  {
+    id: 11,
+    name: 'Beach Blanket',
+    description: 'Keeps to sand away',
+    price: 11
+  },
+  {
+    id: 12,
+    name: 'Snorkel Mask',
+    description: 'Go for a swim!',
+    price: 7
+  }
+]
