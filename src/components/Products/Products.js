6bfec6cf0900e68609f08a328ae08c74c0d13d03
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2019-04-25 13:42:43 -0600

    see list of products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,15 +3,39 @@ import Product from '../Product/Product';
 import { rawData } from '../../raw_data';
 
 export default class Products extends Component {
-  state = {
-    products: rawData
+  constructor() {
+    super();
+    this.state = {
+      products: null
+    }
   }
+
+  componentDidMount() {
+    this.setState({
+      products: rawData
+    });
+  }
+
+  returnProducts = () => {
+    return this.state.products.map((product, i) => {
+      return(
+        <div key={i}>
+          <p>{product.name}</p>
+        </div>
+      )
+    });
+  }
+
   render() {
     return (
       <>
         <div className='py-5'>
           <div className='container'>
             <div className='row'>
+             {
+               this.state.products !== null ? this.returnProducts() : null
+               // this.returnProducts()
+             }
             </div>
           </div>
         </div>
